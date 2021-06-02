206f439123f872e73b7ae1fe0c94b6495ca626ba
sibalzer <github@simonbalzer.de>
2021-06-02 03:43:14 +0200

    finished new settings.py

--- src/common.py
+++ src/common.py
@@ -10,7 +10,7 @@ APPOINTMENT_URL = r"https://www.impfportal-niedersachsen.de/portal/#/appointment
 MAIL_REGEX = r"\b(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])\b"
 ZIP_REGEX = r"(19|21|26|27|28|29|30|31|34|37|38|48|49)([0-9]{3})"
 BIRTHDATE_REGEX = r"^[0-3]?[0-9]\.[0-3]?[0-9]\.(?:[0-9]{2})?[0-9]{2}$"
-TIME_REGEX = r"[0-9]*(?:\.[0-9])?$"
+NUMBER_REGEX = r"[0-9]*(?:\.[0-9])?$"
 
 BOOL_REGEX = r"(?i)(?:true)|(?:false)"
 
@@ -40,6 +40,7 @@ def is_night() -> bool:
     now = datetime.now().time()
     return not (morning < now and now < evening)
 
+
 GOOD_PLZ = ['19', '21', '26', '27', '28', '29',
             '30', '31', '34', '37', '38', '48', '49']
 
@@ -49,10 +50,11 @@ def sleep(time_in_s: int, jitter: int = 0) -> None:
     start = datetime.now()
     end = datetime.now() + timedelta(seconds=time_to_wait)
     while end > datetime.now():
-        elapsed_time_s: int = (datetime.now() - start).total_seconds()
-        time_to_wait_s: int = time_to_wait
+        elapsed_time_s = (datetime.now() - start).total_seconds()
+        time_to_wait_s = time_to_wait
+        elapsed_time_delta = time_to_wait_s - elapsed_time_s
 
-        time_str = f"\rSleeping ({elapsed_time_s}s/{time_to_wait_s}s)"
+        time_str = f"\rSleeping ({int(elapsed_time_s)}s/{int(time_to_wait_s)}s)"
         sys.stdout.write(time_str)
         sys.stdout.flush()
 
@@ -73,5 +75,5 @@ def sleep_until(hour, minute) -> None:
 
 
 def datetime2timestamp(dt):
-    return int(datetime.now().timestamp() -
-               (datetime.now() - dt).total_seconds())
+    return int(datetime.now().timestamp()
+               - (datetime.now() - dt).total_seconds())
